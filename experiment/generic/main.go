0eff8e251fbd156cda5bc0658e9fbe75bb065e12
LT Tsai <lt@ltt.pw>
2022-09-03 17:56:22 +0800

    chore(experiment): add generic experiment

---
+++ experiment/generic/main.go
@@ -0,0 +1,17 @@
+package main
+
+type Subscriber[T any] interface {
+	Subscribe(addr string) chan T
+}
+
+type mySubscriber[T any] struct{}
+
+func (sub mySubscriber[int]) Subscribe(addr string) chan int {
+	ch := make(chan int)
+	return ch
+}
+
+func main() {
+	var subscriber Subscriber[int] = mySubscriber[int]{}
+	subscriber.Subscribe("test")
+}
